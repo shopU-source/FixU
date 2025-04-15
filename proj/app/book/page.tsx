7fef7b5c364990243be69d9db52f94e279dd738c
arbazahmed07 <mdarbazking7@gmail.com>
2025-04-15 22:46:07 +0530

    service req model added

---
+++ proj/app/book/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react';
+import ServiceForm from "../components/ServiceForm"
+const Book: React.FC = () => {
+  return (
+   <ServiceForm/>
+  )
+}
+
+export default Book
